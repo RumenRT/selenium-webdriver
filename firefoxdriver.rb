90f950c83429daccfa7dba77db352c1cafa4d171
RumenRT <33309rt@gmail.com>
2019-04-22 15:18:52 -0400

    initial

---
+++ firefoxdriver.rb
@@ -0,0 +1,39 @@
+# require selenium-webdriver
+require 'selenium-webdriver'
+
+# launch firefox browser
+driver = Selenium::Webdriver.for :firefox
+
+# maximize window
+driver.manage.window.maximize
+
+# navigate to the website
+driver.get 'http://www.newtours.demoaut.com/'
+
+# find element for username
+username = driver.find_element(:name, 'userName')
+
+# type selenium in username text box
+username.send_keys('selenium')
+
+# find password textbox
+password = driver.find_element(:name, 'password')
+
+# type selenium into password textbox
+password.send_keys('selenium')
+
+sign_in_button = driver.find_element(:name, 'login')
+sign_in_button.click
+sleep(*duration 3)
+
+# validate/assert if title of next page is correct
+puts driver.title
+sleep(*duration 3)
+if driver.title == 'Find a Flight: Mercury Tours:'
+  puts 'Test passed'
+else
+  raise 'Test failed'
+end
+
+
+
